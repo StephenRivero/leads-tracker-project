d49c2443824fd6e681b54e6e7cf04569df3a286f
Stephen Manasseh Rivero Pastor <stephen.m.rivero@gmail.com>
2022-10-16 09:29:14 +0800

    Add render function

---
+++ index.js
@@ -0,0 +1,27 @@
+// "www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+console.log(ulEl)
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+
+    for (i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        // let li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+
+    ulEl.innerHTML = listItems
+}
+
